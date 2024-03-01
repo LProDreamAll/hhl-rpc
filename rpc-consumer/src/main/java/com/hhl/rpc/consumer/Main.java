95c8dec5bd58ce046a815cf1d07edbb67d5dca33
lihanghang <lihanghang@guazi.com>
2024-03-01 15:28:25 +0800

    初始化代码

---
+++ rpc-consumer/src/main/java/com/hhl/rpc/consumer/Main.java
@@ -0,0 +1,7 @@
+package com.hhl.rpc.consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ rpc-facade/src/main/java/com/hhl/rpc/facade/Main.java
@@ -0,0 +1,7 @@
+package com.hhl.rpc.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
